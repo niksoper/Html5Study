1b22ecffe910a318c3a5961e4904716ec73a89f4
niksoper <nsoper@scottlogic.com>
2015-09-11 16:18:32 +0100

    Some more playing with events

--- Html5/scripts/events.js
+++ Html5/scripts/events.js
@@ -4,21 +4,32 @@
 
     var handlers = new Map()
     
-    document.addEventListener('click', handler('document'), false)
+    document.addEventListener('click', clickHandler('document'), false)
     handleClickForAll('button', document.getElementsByTagName('button'))
     handleClickForAll('input', document.getElementsByTagName('input'))
     handleClickForAll('fieldset', document.getElementsByTagName('fieldset'))
     handleClickForAll('.invalid', document.getElementsByClassName('invalid'))
-
     
-    function handler(name) {
+    let form = document.querySelector('form')
+    
+    document.querySelector('input[list]').addEventListener('change', function() {
+       console.log('Second input changed') 
+       console.dir(form)
+       form.dispatchFormChange()
+    });
+    
+    document.querySelector('input').addEventListener('formchange', function() {
+       console.log('Form change handled by first input with value of ${this.value}') 
+    });
+    
+    function clickHandler(name) {
         let f = function (e) {
-            console.log('Handler for ' + name)
+            console.log('Click handler for ' + name)
             //dirLog('this', this)
             //dirLog('e', e)
             e.preventDefault()
             e.stopPropagation()
-            this.removeEventListener('click', handlers.get(name), false)
+            //this.removeEventListener('click', handlers.get(name), false)
         }
         
         handlers.set(name, f)
@@ -28,7 +39,7 @@
 
     function handleClickForAll(name, collection) {
         for (let i = 0; i < collection.length; i++) {
-            collection[i].addEventListener('click', handler(name + i), false)
+            collection[i].addEventListener('click', clickHandler(name + i), false)
         }
     }
 
