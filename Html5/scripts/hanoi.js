d7a11742d266b8ece479916163b74ca29927cbf4
niksoper <nsoper@scottlogic.com>
2015-09-16 15:34:00 +0100

    Positioning discs correctly using css transform via javasript

--- Html5/scripts/hanoi.js
+++ Html5/scripts/hanoi.js
@@ -1,4 +1,4 @@
-﻿(function ($) {
+﻿$().ready(function() {
 
     'use strict';
 
@@ -6,16 +6,21 @@
         forEach = Array.prototype.forEach,
         targetClass = 'over',
         pegs = document.getElementsByClassName('peg'),
-        draggables = document.querySelectorAll('.disc'),
-        sourceElement = null;
+        discs = document.querySelectorAll('.disc'),
+        sourceElement = null,
+        DISC_HEIGHT = 10,
+        PEG_WIDTH = 15,
+        PEG_HEIGHT = 250;
 
-    forEach.call(draggables, function (draggable) {
-        draggable.addEventListener('dragend', dragEnd, false);
-        draggable.addEventListener('dragstart', dragStart, false);
-        draggable.addEventListener('mouseover', mouseOver, false);
+    forEach.call(discs, function (disc) {
+        setWidth(disc);
+        disc.addEventListener('dragend', dragEnd, false);
+        disc.addEventListener('dragstart', dragStart, false);
+        disc.addEventListener('mouseover', mouseOver, false);
     });
 
     forEach.call(pegs, function (peg) {
+        positionDiscs(peg);
         peg.addEventListener('dragenter', cancel, false);
         peg.addEventListener('dragover', dragOver, false);
         peg.addEventListener('dragleave', dragLeave, false);
@@ -34,6 +39,10 @@
         cancel(e);
         e.target.appendChild(sourceElement);
         sourceElement = null;
+
+        forEach.call(pegs, function (peg) {
+            positionDiscs(peg);
+        });
     }
 
     function dragOver(e) {
@@ -56,4 +65,18 @@
         e.preventDefault();
     }
 
-})($);
+    function setWidth(disc) {
+        var discWidth = Number(disc.getAttribute('data-discsize')) * 37.5;
+        $(disc).width(discWidth + 'px');
+    }
+
+    function positionDiscs(peg) {
+        var dy = PEG_HEIGHT - (peg.childElementCount * DISC_HEIGHT),
+            dx = null;
+        forEach.call(peg.children, function (disc, index) {
+            dx = ($(disc).width() / -2) + (PEG_WIDTH / 2);
+            disc.style.transform = 'translate(' + dx + 'px, ' + dy + 'px)';
+        });
+    }
+
+});
